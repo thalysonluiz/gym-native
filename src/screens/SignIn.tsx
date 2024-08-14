a394fa20263c6c907ac9e7f841346fb767ca7545
Thalyson Luiz <thalysonluiz@gmail.com>
2024-08-14 13:22:30 -0300

    feat: heading signin

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+
+import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
 
 export function SignIn() {
   return (
-    <View className="flex-1 justify-center items-center bg-slate-400">
-      <Text className="text-red-600">dd!</Text>
+    <View className="flex-1 items-center bg-gray-700">
+      <Image 
+        source={BackgroundImg} 
+        className="absolute top-0" 
+        resizeMode="contain"
+        alt="Pessoas treinando em bicicleta"
+      />
+      <View className="my-24">
+        <LogoSvg />
+        <Text className="text-gray-100 text-sm">Treine sua mente e o seu corpo</Text>
+      </View>
+      <Text className="text-white text-xl font-heading mb-6">Acesse sua conta</Text>
     </View>
   )
 }
