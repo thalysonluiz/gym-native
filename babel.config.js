8dd382745b7d4dde0542e68df8e55b3884d55829
Thalyson Luiz <thalysonluiz@gmail.com>
2024-08-14 12:17:56 -0300

    chore: config nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: "nativewind" }],
+      "nativewind/babel"
+    ],
     plugins: [
       [
         'module-resolver',
@@ -21,6 +24,7 @@ module.exports = function(api) {
           }
         },
       ],
+      "react-native-reanimated/plugin",
     ],
   };
 };
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function SignIn() {
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-400">
+      <Text className="text-red-600">dd!</Text>
+    </View>
+  )
+}
