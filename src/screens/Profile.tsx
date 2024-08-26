4cb5478b2a10257b86bc58310268ff2ed512d5ea
Thalyson Luiz <thalysonluiz@gmail.com>
2024-08-26 14:35:25 -0300

    feat: return name and email hook form

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,17 +1,53 @@
 import { useState } from "react";
 import { Alert, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
 import * as ImagePicker from "expo-image-picker"
 import Toast from 'react-native-toast-message';
+
+import { useAuth } from "@hooks/useAuth";
 
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  old_password: string;
+  password_confirm: string;
+}
+
+const FormValidationSchema = z.object({
+  password: z.string({message: "Senha é obrigatória"}).min(6, {message: "Senha mínima de 6 caracteres"}),
+  password_confirm: z.string({message: "Confirmação de Senha obrigatória"}),
+})
+.refine(({ password_confirm, password }) => password === password_confirm, {
+    message: "Senhas não conferem",
+    path: ["password_confirm"]
+  }  
+);
+
 const PHOTO_SIZE = 130
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
   const [userPhoto, setUserPhoto] = useState('https://github.com/thalysonluiz.png')
+
+  const { user } = useAuth()
+
+  const { control, 
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: zodResolver(FormValidationSchema),
+    defaultValues: {
+      name: user?.name,
+      email: user?.email,
+    }
+  })
 
   async function handleUserPhotoSelect(){
     setPhotoIsLoading(true)
@@ -60,8 +96,34 @@ export function Profile() {
               Alterar foto
             </Text>
           </TouchableOpacity>
-          <Input style={{backgroundColor: '#202024'}} placeholder="Nome" />
-          <Input style={{backgroundColor: '#202024'}} value="Email" editable={false} selectTextOnFocus={false} />
+
+          <Controller
+            control={control}
+            name="name"
+            render={({field: {onChange, value }}) => (
+              <Input 
+                style={{backgroundColor: '#202024'}} 
+                placeholder="Nome" 
+                onChangeText={onChange}
+                value={value}
+              />
+            )} 
+          />
+
+          <Controller
+            control={control}
+            name="email"
+            render={({field: {onChange, value }}) => (
+              <Input 
+                style={{backgroundColor: '#202024', color: '#7C7C8A'}} 
+                value={value} 
+                editable={false} 
+                selectTextOnFocus={false} 
+                onChangeText={onChange}
+              />
+            )} 
+          />
+          
         </View>
         <View className="px-10 mt-12 mb-9">
             <Text className="text-gray-200 font-heading text-base mb-2">
